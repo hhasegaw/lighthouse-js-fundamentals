c118788cc928a1d8e790ad3b48be421ee3c27feb
Haruna Hasegawa <hhasegawa@Daniels-MacBook.local>
2018-08-13 01:50:03 -0400

    Adds looping exercises

--- chorus-while.js
+++ chorus-while.js
@@ -4,15 +4,4 @@ while (repeat < 10) {
   console.log(chorus);
   repeat++;
 }
-console.log("Until the sun comes up!");
-
-var chorus = "Let's dance!";
-var repeat = 0;
-while (repeat < 10) {
-  if (repeat === 5) {
-    console.log("*change key*");
-  }
-  console.log(chorus);
-  repeat++;
-}
-console.log("Until the sun comes up!");
+console.log("Until the sun comes up!")
